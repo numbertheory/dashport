e06d696465713f2331c1781a672c24b11eb7c560
JP Etcheber <jetcheber@gmail.com>
2022-05-20 21:46:03 -0700

    Add skeleton for wrapper

---
+++ setup.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='dashport',
+    use_scm_version=True,
+    description='Dashport curses wrapper for Python',
+    long_description=readme,
+    author='JP Etcheber',
+    author_email='jetcheber@gmail.com',
+    url='https://github.com/numbertheory/dashport',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
