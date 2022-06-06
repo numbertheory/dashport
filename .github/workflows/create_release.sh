f94c093955060278c1de897408983c158844acc0
JP Etcheber <jetcheber@gmail.com>
2022-06-05 23:24:04 -0700

    get rid of releases process

--- .github/workflows/create_release.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-last_tag=$(git tag --sort=committerdate -l | tail -2 | head -1)
-current_tag=$(git describe --tags)
-commits=$(git log $current_tag...$last_tag --oneline --pretty='format:%C(auto)%h %s')
-
-echo $commits
